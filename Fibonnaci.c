f0d2694ca89d95d00102534440a8ac1c07c8efba
Priyanka Pandey <110595977+603977@users.noreply.github.com>
2023-06-30 23:18:10 +0530

    Create Fibonnaci.c

---
+++ Fibonnaci.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    int num1=0,num2=1,i,num3, value ;
+    printf("enter the number of elements ");
+    scanf("%d",&value);
+    printf("\n %d",n1);
+     printf("\n %d",n2);
+    for(i=2;i<n;i++)
+    {
+        n3=n1+n2;
+        printf("\n %d",n3);
+        n1=n2;
+        n2=n3;
+        
+    }
+    return 0 ;
